1d00a7c4e8139e7b28e3a8e1a9135254ce14df93
Avery (Hee-Woon) Ryoo <54091374+averyryoo@users.noreply.github.com>
2020-05-27 00:04:32 -0400

    uploaded weekly assignment files

---
+++ wa05/wa05.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+
+using static System.Console;
+
+namespace Bme121
+{
+    // Describe one subject in study of males being treated for inoperable lung cancer.
+
+    class Subject
+    {
+        public string ChemoType       { get; private set; } // Type of chemotherapy
+        public string CellType        { get; private set; } // Type of cancer cells
+        public int    SurvivalDays    { get; private set; } // Suvival time in days
+        public bool   IsCensored      { get; private set; } // Indicate censored survival times
+        public int    KarnofskyPct    { get; private set; } // Karnofsky performance percent
+        public int    DiagMonthsAgo   { get; private set; } // Months since diagnosis
+        public int    AgeYears        { get; private set; } // Age in years
+        public bool   HadPriorTherapy { get; private set; } // Indicate therapy prior to trial
+
+        // Private default constructor so users can't instantiate objects.
+
+        private Subject( ) { }
+
+        // Return new object with data contained in csv string.
+
+        private static Subject ParseCsv( string csv )
+        {
+            Subject result = new Subject( );
+            
+            string[ ] parts = csv.Split ( ',' );
+
+            
+            result.ChemoType = parts[0];
+            result.CellType = parts[1]; 
+            result.SurvivalDays = int.Parse(parts[2]); 
+            result.IsCensored = YesNoParse(parts[3]); 
+            result.KarnofskyPct = int.Parse(parts[4]); 
+            result.DiagMonthsAgo = int.Parse(parts[5]); 
+            result.AgeYears = int.Parse(parts [6]); 
+            result.HadPriorTherapy = YesNoParse(parts [7]); 
+            
+            return result;
+        }
+
+        // Return true/false for yes/no.
+
+        private static bool YesNoParse( string s )
+        {
+            if( s == "yes" ) return  true;
+            if( s == "no"  ) return false;
+            throw new FormatException( "Invalid yes/no format" );
+        }
+
+        // Return a short description for debugging.
+
+        public override string ToString( )
+        {
+            string chemoPart = "???";
+            if( ChemoType == "standard" ) chemoPart = "Std";
+            if( ChemoType == "test"     ) chemoPart = "Tst";
+
+            string cellPart = "??";
+            if( CellType == "squamous"  ) cellPart  = "Sq";
+            if( CellType == "smallcell" ) cellPart  = "Sc";
+            if( CellType == "adeno"     ) cellPart  = "Ad";
+            if( CellType == "large"     ) cellPart  = "Lg";
+
+            return string.Format( "{0}{1}{2:d3}", chemoPart, cellPart, SurvivalDays );
+        }
+
+        public static Subject[ ] MakeArrayFromFile( string path )
+        {
+			Subject[ ] result;
+            using( FileStream file = new FileStream(path, FileMode.Open, FileAccess.Read ) )
+            { 
+				using( StreamReader reader = new StreamReader( file ) )
+				{
+					reader.ReadLine( );
+					int count = 0;
+					while( ! reader.EndOfStream )
+					{
+						reader.ReadLine();
+						count ++;
+					}
+					result = new Subject[ count ];
+					
+					file.Position = 0;
+					
+					reader.ReadLine( );
+					
+					int i = 0;
+					while ( ! reader.EndOfStream )
+					{
+						result[ i ] = ParseCsv( reader.ReadLine( ) );
+						i ++;
+					}
+				} 
+			}
+			return result;
+        }
+    }
+
+    static class Program
+    {
+        static void Main( )
+        {
+            Subject[ ] subjects = Subject.MakeArrayFromFile( "lung.csv" );
+
+            for( int i = 0; i < subjects.Length; i ++ )
+            {
+                if( i % 10 > 0 ) Write( " " );
+                Write( subjects[ i ] );
+                if( i % 10 == 9 ) WriteLine( );
+            }
+        }
+    }
+}
+
---
+++ wa07/wa07.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+using MediCal;
+using DataStructures;
+
+namespace DataStructures
+{
+	delegate bool Comparison(Drug d);
+    partial class LinkedList
+    {
+        // Method forms a sub-list of Drug objects satisfying a target predicate.
+        public LinkedList SubList(Comparison compare)
+        {
+			// establish new Linked List
+            LinkedList drugList = new LinkedList();
+            
+            // establish head
+            Node currentNode = Head;
+            
+            // add to linked List
+            while(currentNode.Next != null)
+            {
+				if(compare(currentNode.Data))
+				{
+					drugList.AddLast(currentNode.Data);
+				}
+				currentNode = currentNode.Next;
+			}
+			// return linked list
+			return drugList;
+        }
+    }
+}
+
+namespace Bme121
+{
+    static class Program
+    {
+        // Test a drug name for Lorazepam tablet.
+        public static bool IsLorazepamTablet( Drug d )
+        {
+            return d.Name.Contains( "LORAZEPAM" ) && d.Name.Contains( "TABLET" );
+        }
+
+        // Test a drug name for Temazipam capsule.
+        public static bool IsTemazipamCapusule( Drug d )
+        {
+            return d.Name.Contains( "TEMAZEPAM" ) && d.Name.Contains( "CAPSULE" );
+        }
+
+        // Test the 'LinkedList.SubList' method.
+        static void Main( )
+        {
+            // Parameters for reading the drug file.
+            const string path = "RXQT1503.txt";
+            const FileMode mode = FileMode.Open;
+            const FileAccess access = FileAccess.Read;
+
+            // Load all drugs into a linked list.
+            LinkedList allDrugs = new LinkedList( );
+            using( FileStream file = new FileStream( path, mode, access ) )
+            using( StreamReader reader = new StreamReader( file ) )
+            {
+                while( ! reader.EndOfStream )
+                {
+                    string line = reader.ReadLine( );
+                    Drug d = Drug.ParseFileLine( line );
+                    allDrugs.AddLast( d );
+                }
+            }
+            WriteLine( "allDrugs.Count = {0}", allDrugs.Count );
+
+            // Form a sub-list of Temazipam capsules.
+            LinkedList subDrugs1 = allDrugs.SubList(IsTemazipamCapusule);
+            WriteLine( "subDrugs1.Count = {0}", subDrugs1.Count );
+            foreach( Drug d in subDrugs1.ToArray( ) ) WriteLine( d );
+
+            // Form a sub-list of Lorazepam tablets.
+            LinkedList subDrugs2 = allDrugs.SubList(IsLorazepamTablet);
+            WriteLine( "subDrugs2.Count = {0}", subDrugs2.Count );
+            foreach( Drug d in subDrugs2.ToArray( ) ) WriteLine( d );
+        }
+    }
+}
